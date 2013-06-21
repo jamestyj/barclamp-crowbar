4d0fef31a230d489e795b029fb77b3b07fdcb80c
James Tan <jatan@suse.de>
2013-06-21 11:23:11 +0200

    [Docs] Fix up the Help navigation and page
     * Site navigation menu should have consistent behavior and bring you to
       pages, rather than triggering PDF downloads. Remove sub-menu items
       and just have a simple page providing the relevant links to both HTML
       and PDF docs.
     * Update external wiki link from the old dellcloudedge Github
       organization to crowbar. Also add warning that external wiki contains
       information for a different version.
     * Remove unused code that generates (incomplete) HTML docs.
     * Remove outdated, Dell specific PDF docs.
     * Add symlinks to SUSE Cloud PDF documentation. Requires the
       susecloud-{admin,deployment,user}_en-pdf RPMs to be installed.
     * Add symlinks to SUSE Cloud HTML documentation. Requires the
       susecloud-manuals_en RPM to be installed.

--- crowbar_framework/config/navigation.rb
+++ crowbar_framework/config/navigation.rb
@@ -19,12 +19,6 @@ SimpleNavigation::Configuration.run do |navigation|
       secondary.item :util_index, t('nav.util_logs'), utils_path 
       # insert here for :utils
     end
-    primary.item :help, t('nav.help'), '/crowbar_users_guide.pdf', { :link => { :target => "_blank" } } do |secondary|
-      secondary.item :help, t('nav.crowbar_wiki'), 'https://github.com/dellcloudedge/crowbar/wiki/', { :link => { :target => "_blank" } }
-      secondary.item :help, t('nav.crowbar_ug'), '/crowbar_users_guide.pdf', { :link => { :target => "_blank" } }
-      secondary.item :help, t('nav.crowbar_dg'), '/crowbar_deployment_guide.pdf', { :link => { :target => "_blank" } }
-      secondary.item :documentation, t('nav.documentation'), docs_path if RAILS_ENV == 'development'
-      # insert here for :help 
-    end
+    primary.item :help, t('nav.help'), docs_path
   end
 end
